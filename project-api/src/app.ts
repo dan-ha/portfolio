670ec9b9fe2966eff43f6ffd816f2e58e3c8f45e
danha <daniel.hamarik@gmail.com>
2019-05-14 17:57:38 +0200

    env support

--- project-api/src/app.ts
+++ project-api/src/app.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as dotenv from 'dotenv'
 
 import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
@@ -8,47 +9,69 @@ import * as expressWinston from 'express-winston';
 import * as swaggerUi from 'swagger-ui-express';
 const swaggerDocument = require('../api/projectapi.json');
 
-import { Index } from './routes/index';
 import ProjectRoute from './routes/project';
 import * as errorHandler from './utility/errorHandling';
 
 class App {
-    public app: express.Application;
-    public IndexRoutes: Index = new Index();
-    private projectRoute: ProjectRoute = new ProjectRoute();
-    public mongoUrl: string = 'mongodb://localhost/projects'
+  public app: express.Application;
+  private projectRoute: ProjectRoute = new ProjectRoute();
+  public mongoUrl: string
+  public mongoUser: string
+  public mongoPass: string
 
-    constructor() {
-        this.app = express();
-        this.app.use(bodyParser.json());
+  constructor() {
+    const path = `${__dirname}/../.env.${process.env.NODE_ENV}`
+    dotenv.config({ path: path })
+    this.mongoUrl = `mongodb://${process.env.MONGODB_URL_PORT}/${
+      process.env.MONGODB_DATABASE
+    }`;
+    this.mongoUser = `${process.env.MONGODB_USER}`
+    this.mongoPass = `${process.env.MONGODB_PASS}`
 
-        // Swagger-UI
-        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    this.app = express();
+    this.app.use(bodyParser.json());
 
-        // Routes
-        this.IndexRoutes.routes(this.app);
-        this.projectRoute.routes(this.app);
+    // Swagger-UI
+    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-        // Database
-        this.mongoSetup();
+    // Routes
+    this.projectRoute.routes(this.app);
 
-        // Logging
-        this.app.use(
-          expressWinston.errorLogger({
-            transports: [new winston.transports.Console()],
-          })
-        );
-        // Error handling
-        this.app.use(errorHandler.logging);
-        this.app.use(errorHandler.errorHandler);
-    }
+    // Database
+    this.mongoSetup();
 
-    private mongoSetup(): void {
-        mongoose.connect(
-          this.mongoUrl,
-          { useNewUrlParser: true }
-        )
+    // Logging
+    this.app.use(
+      expressWinston.errorLogger({
+        transports: [new winston.transports.Console()],
+      })
+    );
+    // Error handling
+    this.app.use(errorHandler.logging);
+    this.app.use(errorHandler.errorHandler);
+  }
+
+  private mongoSetup(): void {
+    let options
+
+    if (process.env.NODE_ENV !== 'prod') {
+      options = {
+        useNewUrlParser: true,
       }
+    } else {
+      options = {
+        user: this.mongoUser,
+        pass: this.mongoPass,
+        useNewUrlParser: true,
+      }
+    }
+    mongoose.connect(
+      this.mongoUrl,
+      options
+    )
+  }
 }
 
 export default new App().app;
+
+
---
+++ project-api/startdb.sh
@@ -0,0 +1,1 @@
+docker run --name my-mongo -p 27017:27017 -v `pwd`/data/mongo:/data/db -d mongo:latest
